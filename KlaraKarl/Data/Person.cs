141f268a8bd19f40c68268d992eca36458412ecd
ersk02 <erik.skold@regionvarmland.se>
2023-01-03 08:08:35 +0100

    Massa fixes

--- KlaraKarl/Data/Person.cs
+++ KlaraKarl/Data/Person.cs
@@ -1,8 +1,10 @@
-﻿namespace KlaraKarl.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KlaraKarl.Data
 {
     public class Person
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string? Surname { get; set; }
         public string? Lastname { get; set; }
         public string? Email { get; set; }
