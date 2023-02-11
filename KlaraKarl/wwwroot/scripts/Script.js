25ed7bddacfbfee65ac26f134a90bfc462ede154
Erik S <erik9996@hotmail.com>
2023-02-11 16:43:49 +0100

    css and script

---
+++ KlaraKarl/wwwroot/scripts/Script.js
@@ -0,0 +1,14 @@
+﻿
+//window.getTitle = () => {
+//    return document.title;
+//};
+function hidehomebtn() {
+    if (document.getElementById("heart")) {
+        document.getElementById("homebtn").style.visibility = "hidden";
+
+    }
+    else {
+        document.getElementById("homebtn").style.visibility = "visible";
+
+    }
+}
