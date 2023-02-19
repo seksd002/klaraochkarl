b49b2bb0768e52e9a4a69cbd3e228625a2a81f60
Erik S <erik9996@hotmail.com>
2023-02-19 11:02:47 +0100

    Design onvalid and javascript

--- KlaraKarl/wwwroot/scripts/Script.js
+++ KlaraKarl/wwwroot/scripts/Script.js
@@ -2,13 +2,3 @@
 //window.getTitle = () => {
 //    return document.title;
 //};
-function hidehomebtn() {
-    if (document.getElementById("heart")) {
-        document.getElementById("homebtn").style.visibility = "hidden";
-
-    }
-    else {
-        document.getElementById("homebtn").style.visibility = "visible";
-
-    }
-}
