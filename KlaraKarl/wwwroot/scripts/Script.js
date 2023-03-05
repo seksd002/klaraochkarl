029fdd62cf71eb77918338c59d46d12039ba2566
Erik S <erik9996@hotmail.com>
2023-03-05 21:14:38 +0100

    Urklipp

--- KlaraKarl/wwwroot/scripts/Script.js
+++ KlaraKarl/wwwroot/scripts/Script.js
@@ -2,3 +2,13 @@
 //window.getTitle = () => {
 //    return document.title;
 //};
+window.clipboardCopy = {
+    copyText: function(text) {
+        navigator.clipboard.writeText(text).then(function () {
+            alert("Maillista kopierad till urklipp");
+        })
+        .catch(function (error) {
+            alert(error);
+        });
+    }
+};
